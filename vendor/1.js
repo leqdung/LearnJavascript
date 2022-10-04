e8ef95626d1f292c004945d1a17bcbbdd2f61037
leqdung <dzung.dg@gmail.com>
2022-10-04 14:41:25 +0700

    dulieuhtml5

--- vendor/1.js
+++ vendor/1.js
@@ -1,17 +1,36 @@
+// document.addEventListener(
+//   "DOMContentLoaded",
+//   function () {
+//     //
+//     var tamgiac = document.getElementsByClassName("tamgiac");
+//     var tamgiac = tamgiac[0];
+//     var danhsach = document.getElementsByClassName("danhsach");
+//     var danhsach = danhsach[0];
+//     // xu ly su kien dung onclick va toggle class cho tam giac doi mau
+//     tamgiac.onclick = function () {
+//       // this.style.color = "white"; // tác dụng và thuộc tính style.color
+//       this.classList.toggle("tamgiactrang");
+//       danhsach.classList.toggle("ra");
+//     };
+//   },
+//   false
+// );
+// Ket thuc bat su kien nut fb
+
+// Lấy dữ liệu từ html5
+
 document.addEventListener(
   "DOMContentLoaded",
   function () {
     //
-    var tamgiac = document.getElementsByClassName("tamgiac");
-    var tamgiac = tamgiac[0];
-    var danhsach = document.getElementsByClassName("danhsach");
-    var danhsach = danhsach[0];
-    // xu ly su kien dung onclick va toggle class cho tam giac doi mau
-    tamgiac.onclick = function () {
-      // this.style.color = "white"; // tác dụng và thuộc tính style.color
-      this.classList.toggle("tamgiactrang");
-      danhsach.classList.toggle("ra");
-    };
+    var n1 = document.getElementsByClassName("nuttongquat");
+    // console.log(n1);
+    for (let i = 0; i < n1.length; i++) {
+      n1[i].onclick = function () {
+        // console.log(n1);
+        console.log(this.getAttribute("data-matkhau"));
+      };
+    }
   },
   false
 );
